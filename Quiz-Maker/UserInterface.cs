1cd6d7cd20537b32e6a636eaa2ae1276805a0d17
Kiwicase <emailcaseydent@gmail.com>
2022-10-04 12:22:36 +0300

    Add project files.

---
+++ Quiz-Maker/UserInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_Maker
+{
+    internal class UserInterface
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Casey's Quintessential Quiz Maker");
+        }
+
+        public int HowManyQuestions()
+        {
+            int answer = 0;
+            Console.WriteLine("How many questions would you like in your Quiz? \n5 is the maximum questions allowed: ");
+            string input = Console.ReadLine();
+            while (!Int32.TryParse(input, out answer) || answer > 5)
+            {
+                Console.WriteLine("This is not a valid input. \nHow many questions would you like in your Quiz? \n5 is the maximum questions allowed: ");
+                input = Console.ReadLine();
+            }
+            return answer;
+        }
+
+        public string TypeYourQuestions()
+        {
+            Console.WriteLine("Please type your question, followed by 4 answers: ");
+            string firstQuestion = Console.ReadLine();
+            return firstQuestion;
+        }
+    }
+}
