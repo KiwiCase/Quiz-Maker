4c7e7311ca1231d8e0582a749a802ae52b538665
Kiwicase <emailcaseydent@gmail.com>
2022-10-07 12:51:47 +0300

    Question Class created

---
+++ Quiz-Maker/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_Maker
+{
+    public class Question
+    {
+        public string firstQuestion;
+        public string AnswerOne;
+        public string AnswerTwo;
+        public string AnswerThree;
+        public string AnswerFour;
+        public int CorrectAnswer;
+    }
+}
