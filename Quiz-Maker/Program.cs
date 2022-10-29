6a6bc813238217eda37cf6a4d98c98567a08aef5
Kiwicase <emailcaseydent@gmail.com>
2022-10-29 14:39:46 +0300

    If statements added for multiple user questions and answers

--- Quiz-Maker/Program.cs
+++ Quiz-Maker/Program.cs
@@ -7,8 +7,30 @@
             UserInterface.WelcomeMessage();
             int answer = UserInterface.HowManyQuestions();
 
-            QuestionAndAnswers QnAOne = UserInterface.GetQuestionAndAnswers();
-          
+            if (answer < 6)
+            {
+                QuestionAndAnswers QnAOne = UserInterface.GetQuestionAndAnswers();
+            }
+
+            if (answer < 5)
+            {
+                QuestionAndAnswers QnATwo = UserInterface.GetQuestionAndAnswers();
+            }
+
+            if (answer < 4)
+            {
+                QuestionAndAnswers QnAThree = UserInterface.GetQuestionAndAnswers();
+            }
+
+            if (answer < 3)
+            {
+                QuestionAndAnswers QnAFour = UserInterface.GetQuestionAndAnswers();
+            }
+
+            if (answer < 2)
+            {
+                QuestionAndAnswers QnAFive = UserInterface.GetQuestionAndAnswers();
+            }
         }
     }
 }
