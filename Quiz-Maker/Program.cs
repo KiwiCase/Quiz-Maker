ffeb4e282d67ce631e790158283c602ba34bf0fd
Kiwicase <emailcaseydent@gmail.com>
2022-10-29 14:46:55 +0300

    Amended if statements - fixed

--- Quiz-Maker/Program.cs
+++ Quiz-Maker/Program.cs
@@ -7,27 +7,27 @@
             UserInterface.WelcomeMessage();
             int answer = UserInterface.HowManyQuestions();
 
-            if (answer < 6)
+            if (answer > 0)
             {
                 QuestionAndAnswers QnAOne = UserInterface.GetQuestionAndAnswers();
             }
 
-            if (answer < 5)
+            if (answer > 1)
             {
                 QuestionAndAnswers QnATwo = UserInterface.GetQuestionAndAnswers();
             }
 
-            if (answer < 4)
+            if (answer > 2)
             {
                 QuestionAndAnswers QnAThree = UserInterface.GetQuestionAndAnswers();
             }
 
-            if (answer < 3)
+            if (answer > 3)
             {
                 QuestionAndAnswers QnAFour = UserInterface.GetQuestionAndAnswers();
             }
 
-            if (answer < 2)
+            if (answer > 4)
             {
                 QuestionAndAnswers QnAFive = UserInterface.GetQuestionAndAnswers();
             }
