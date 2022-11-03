41995d35eba44b5661610c2e4f65a741fc121858
Kiwicase <emailcaseydent@gmail.com>
2022-11-03 08:54:08 +0200

    QnAOne Object amended

--- Quiz-Maker/Program.cs
+++ Quiz-Maker/Program.cs
@@ -10,15 +10,15 @@
             if (answer > 0)
             {
                 QuestionAndAnswers QnAOne = UserInterface.GetQuestionAndAnswers();
-                Console.WriteLine($"Please confirm your question by pressing ENTER: \n\n{QnAOne.userQuestion}");
+                Console.WriteLine($"Thank you for your question and answers - they are as follows:\n\nQuiz Question 1 - {QnAOne.userQuestion}\nCorrect Answer - {QnAOne.correctAnswer}\nFalse Answer 1 - {QnAOne.falseAnswerOne}\nFalse Answer 2 - {QnAOne.falseAnswerTwo}\nFalse Answer 3 - {QnAOne.falseAnswerThree}\n\nPress Enter To Start The Quiz...");
                 Console.ReadLine();
 
             }
 
-            if (answer > 1)
+            /*if (answer > 1)
             {
                 QuestionAndAnswers QnATwo = UserInterface.GetQuestionAndAnswers(); //In response to Issue 4 in GH
-            }
+            }*/
 
         }
 
