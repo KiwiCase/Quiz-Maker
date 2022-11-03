b359075d4bc618296f76ab526439aab91148dcc4
Kiwicase <emailcaseydent@gmail.com>
2022-11-03 11:17:35 +0200

    For loop added for QnAOne Object

--- Quiz-Maker/Program.cs
+++ Quiz-Maker/Program.cs
@@ -7,18 +7,10 @@
             UserInterface.WelcomeMessage();
             int answer = UserInterface.HowManyQuestions();
 
-            if (answer > 0)
-            {
-                QuestionAndAnswers QnAOne = UserInterface.GetQuestionAndAnswers();
-                Console.WriteLine($"Thank you for your question and answers - they are as follows:\n\nQuiz Question 1 - {QnAOne.userQuestion}\nCorrect Answer - {QnAOne.correctAnswer}\nFalse Answer 1 - {QnAOne.falseAnswerOne}\nFalse Answer 2 - {QnAOne.falseAnswerTwo}\nFalse Answer 3 - {QnAOne.falseAnswerThree}");
-                Console.ReadLine();
-
-            }
-
-            /*if (answer > 1)
-            {
-                QuestionAndAnswers QnATwo = UserInterface.GetQuestionAndAnswers(); //In response to Issue 4 in GH
-            }*/
+                for (int i = answer; i == answer; i++)
+                {
+                    QuestionAndAnswers QnAOne = UserInterface.GetQuestionAndAnswers();
+                }
 
         }
 
