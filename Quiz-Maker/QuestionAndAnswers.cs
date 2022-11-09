bde482cb88a0300e77d3d7db10a976da62f4617d
Kiwicase <emailcaseydent@gmail.com>
2022-11-09 15:03:02 +0200

    Start of List of Class elements

--- Quiz-Maker/QuestionAndAnswers.cs
+++ Quiz-Maker/QuestionAndAnswers.cs
@@ -8,11 +8,14 @@ namespace Quiz_Maker
 {
     public class QuestionAndAnswers
     {
+        List<string> QnAList = new List<string>();
         public string userQuestion;
         public string falseAnswerOne;                 //TODO: this could maybe perhaps possilby be a list of string
         public string falseAnswerTwo;                 //TODO: this could maybe perhaps possilby be a list of string
         public string falseAnswerThree;               //TODO: this could maybe perhaps possilby be a list of string
         public string correctAnswer;
+
+        
         private int correctAnswerIndex;//TODO: this could maybe perhaps possilby be a list of string
 
         public override string ToString()
