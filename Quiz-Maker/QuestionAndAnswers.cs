07ac81ae3729a8ffdcb0ec538cf5777d951138fd
Kiwicase <emailcaseydent@gmail.com>
2022-11-11 15:34:25 +0200

    List change, lots of changing code but has not worked so I have reverted back to the last working code

--- Quiz-Maker/QuestionAndAnswers.cs
+++ Quiz-Maker/QuestionAndAnswers.cs
@@ -7,20 +7,18 @@ using System.Threading.Tasks;
 namespace Quiz_Maker
 {
     public class QuestionAndAnswers
+
     {
-
-        List<string> QnAList = new List<string>();
-
-
         public string userQuestion { get; set; }
+
+        List<Type> Answers = new List<Type>();
+
         public string falseAnswerOne { get; set; }          //TODO: this could maybe perhaps possilby be a list of string
         public string falseAnswerTwo { get; set; }        //TODO: this could maybe perhaps possilby be a list of string
         public string falseAnswerThree { get; set; }               //TODO: this could maybe perhaps possilby be a list of string
         public string correctAnswer { get; set; }
 
-        
 
-        
         private int correctAnswerIndex;//TODO: this could maybe perhaps possilby be a list of string
 
 
