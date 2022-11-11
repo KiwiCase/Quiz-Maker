53b41b9bf24c50e93bdb61836033cdc6c66cc75a
Kiwicase <emailcaseydent@gmail.com>
2022-11-11 14:02:54 +0200

    Added get set to class elements

--- Quiz-Maker/QuestionAndAnswers.cs
+++ Quiz-Maker/QuestionAndAnswers.cs
@@ -8,12 +8,17 @@ namespace Quiz_Maker
 {
     public class QuestionAndAnswers
     {
+
         List<string> QnAList = new List<string>();
-        public string userQuestion;
-        public string falseAnswerOne;                 //TODO: this could maybe perhaps possilby be a list of string
-        public string falseAnswerTwo;                 //TODO: this could maybe perhaps possilby be a list of string
-        public string falseAnswerThree;               //TODO: this could maybe perhaps possilby be a list of string
-        public string correctAnswer;
+
+
+        public string userQuestion { get; set; }
+        public string falseAnswerOne { get; set; }          //TODO: this could maybe perhaps possilby be a list of string
+        public string falseAnswerTwo { get; set; }        //TODO: this could maybe perhaps possilby be a list of string
+        public string falseAnswerThree { get; set; }               //TODO: this could maybe perhaps possilby be a list of string
+        public string correctAnswer { get; set; }
+
+        
 
         
         private int correctAnswerIndex;//TODO: this could maybe perhaps possilby be a list of string
