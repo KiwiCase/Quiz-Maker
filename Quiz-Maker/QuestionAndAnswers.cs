d955d2e05e45d787731fadd2fc699e04de03e40f
Kiwicase <emailcaseydent@gmail.com>
2022-11-15 13:20:40 +0200

    Removed commented out code

--- Quiz-Maker/QuestionAndAnswers.cs
+++ Quiz-Maker/QuestionAndAnswers.cs
@@ -23,16 +23,6 @@ namespace Quiz_Maker
 
     }
 
-    /*public string falseAnswerOne { get; set; }          //TODO: this could maybe perhaps possilby be a list of string
-    public string falseAnswerTwo { get; set; }        //TODO: this could maybe perhaps possilby be a list of string
-    public string falseAnswerThree { get; set; }               //TODO: this could maybe perhaps possilby be a list of string
-    public string correctAnswer { get; set; }*/
-
-
-    //private int correctAnswerIndex; //TODO: this could maybe perhaps possilby be a list of string
-
-
-
 }
 
 
