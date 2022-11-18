b44d20d8d4968d91f5546084e8d82323b195c24e
Kiwicase <emailcaseydent@gmail.com>
2022-11-18 13:07:25 +0200

    Attempting to call AskQuestion method to no avail

--- Quiz-Maker/Program.cs
+++ Quiz-Maker/Program.cs
@@ -9,12 +9,16 @@ namespace Quiz_Maker
             UserInterface.WelcomeMessage();
             int answer = UserInterface.HowManyQuestions();
 
+            QuestionAndAnswers userQnA;
+        
             for (int i = 0; i < answer; i++)
             {
-                UserInterface.GetQuestionAndAnswers();
+                userQnA = UserInterface.GetQuestionAndAnswers();
             }
 
             UserInterface.ReadyToPlayQuiz();
+
+            UserInterface.AskQuestion(userQnA);
         }
     }
 }
