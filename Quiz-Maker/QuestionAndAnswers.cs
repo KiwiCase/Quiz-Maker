1ae8f3952e658d8efc92527d285fd1fe52d7aa82
Kiwicase <emailcaseydent@gmail.com>
2022-11-24 10:21:53 +0200

    Removed unused code and class

--- Quiz-Maker/QuestionAndAnswers.cs
+++ Quiz-Maker/QuestionAndAnswers.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Quiz_Maker
 {
     public class QuestionAndAnswers
 
     {
-        private readonly string _correctAnswer;
 
         public QuestionAndAnswers(string question, string correctAnswer)
         {
