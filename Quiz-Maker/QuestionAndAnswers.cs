2dde7f5dd867c5e04febbf5d20bab8817c109d08
KiwiCase <emailcaseydent@gmail.com>
2024-03-28 15:27:34 +0200

    Issue #16 Is this id property needed - resolved.

--- Quiz-Maker/QuestionAndAnswers.cs
+++ Quiz-Maker/QuestionAndAnswers.cs
@@ -7,7 +7,6 @@ namespace Quiz_Maker
         public string Question { get; set; }
         public string CorrectAnswer { get; set; }
         public List<string> IncorrectAnswers { get; }
-        public int id;
 
         public QuestionAndAnswers(string question, string correctAnswer)
         {
